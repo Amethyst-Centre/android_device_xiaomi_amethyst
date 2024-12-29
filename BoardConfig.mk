46e7c06eea23646affccb742630eedb0ddd84a47
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-12-29 18:10:21 +0330

    amethyst: Welcome to OSS world!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,17 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from xiaomi sm8450-common
-include device/xiaomi/sm8450-common/BoardConfigCommon.mk
+# Inherit from xiaomi sm7635-common
+include device/xiaomi/sm7635-common/BoardConfigCommon.mk
 
 # Inherit from the proprietary version
-include vendor/xiaomi/zeus/BoardConfigVendor.mk
+include vendor/xiaomi/amethyst/BoardConfigVendor.mk
 
-DEVICE_PATH := device/xiaomi/zeus
+DEVICE_PATH := device/xiaomi/amethyst
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # Screen density
-TARGET_SCREEN_DENSITY := 560
+TARGET_SCREEN_DENSITY := 446
